be84a5130167b38a2a756e9672ee181648cd10f3
Yidnekachew Kassahun Kibret <yamiyekn@gmail.com>
2022-11-24 21:34:10 +0300

    Add popup.js file

--- JavaScript/popup.js
+++ JavaScript/popup.js
@@ -1,78 +1,79 @@
-const nav = document.getElementById('nav');
-const body = document.getElementById('body');
-const cardWorks = document.getElementById('card-works');
-const modal = document.getElementById('popup'); // modal
-const title = document.getElementById('popup-title'); // text_modal
-const image = document.getElementById('popup-img'); // modal-im
-const longDes = document.getElementById('des');
-const live = document.getElementById('live');
-const source = document.getElementById('source');
+// const nav = document.getElementById("nav");
+// const body = document.getElementById("body");
+const cardWorks = document.getElementById("card-works");
+const modal = document.getElementById("popup");
+const title = document.getElementById("popup-title");
+const image = document.getElementById("popup-img");
+const longDes = document.getElementById("des");
+const live = document.getElementById("live");
+const source = document.getElementById("source");
 
 const cards = [
   {
-    title: 'Tonic',
-    image: './snap-shoot/image1.svg',
+    title: "Tonic",
+    image: "./snap-shoot/image1.svg",
     longDes:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    live: '#',
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    live: "#",
     shortDes:
-      'A daily selection of privately personalized reads; no accounts or sign- ups required.',
-    source: '#',
-    languages: ['html', 'javascript', 'css'],
-    roles: ['Canopy', 'Back End Dev', 2015],
+      "A daily selection of privately personalized reads; no accounts or sign- ups required.",
+    source: "#",
+    languages: ["html", "javascript", "css"],
+    roles: ["Canopy", "Back End Dev", 2015],
   },
   {
-    title: 'Multi-Post Stories',
-    image: './snap-shoot/image2.svg',
+    title: "Multi-Post Stories",
+    image: "./snap-shoot/image2.svg",
     longDes:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    live: '#',
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    live: "#",
     shortDes:
-      'A daily selection of privately personalized reads; no accounts or sign- ups required.',
-    source: '#',
-    languages: ['html', 'javascript', 'css'],
-    roles: ['Canopy', 'Back End Dev', 2015],
+      "A daily selection of privately personalized reads; no accounts or sign- ups required.",
+    source: "#",
+    languages: ["html", "javascript", "css"],
+    roles: ["Canopy", "Back End Dev", 2015],
   },
   {
-    title: 'Tonic',
-    image: './snap-shoot/image3.svg',
+    title: "Tonic",
+    image: "./snap-shoot/image3.svg",
     longDes:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    live: '#',
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    live: "#",
     shortDes:
-      'A daily selection of privately personalized reads; no accounts or sign- ups required.',
-    source: '#',
-    languages: ['html', 'javascript', 'css'],
-    roles: ['Canopy', 'Back End Dev', 2015],
+      "A daily selection of privately personalized reads; no accounts or sign- ups required.",
+    source: "#",
+    languages: ["html", "javascript", "css"],
+    roles: ["Canopy", "Back End Dev", 2015],
   },
   {
-    title: 'Multi-Post Stories',
-    image: './snap-shoot/image4.svg',
+    title: "Multi-Post Stories",
+    image: "./snap-shoot/image4.svg",
     longDes:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    live: '#',
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    live: "#",
     shortDes:
-      'A daily selection of privately personalized reads; no accounts or sign- ups required.',
-    source: '#',
-    languages: ['html', 'javascript', 'css'],
-    roles: ['Canopy', 'Back End Dev', 2015],
+      "A daily selection of privately personalized reads; no accounts or sign- ups required.",
+    source: "#",
+    languages: ["html", "javascript", "css"],
+    roles: ["Canopy", "Back End Dev", 2015],
   },
 ];
 
-function openMobileMenu() {
-  nav.style.display = 'block';
-}
+// function openMobileMenu() {
+//   nav.style.display = 'block';
+// }
 
-function closeMobileMenu() {
-  nav.style.display = 'none';
-}
+// function closeMobileMenu() {
+//   nav.style.display = 'none';
+// }
 
-openMobileMenu();
-closeMobileMenu();
+// openMobileMenu();
+// closeMobileMenu();
 
 // eslint-disable-next-line no-return-assign
 cards.map(
-  (card, index) => (cardWorks.innerHTML += `<div class="card-works1">
+  (card, index) =>
+    (cardWorks.innerHTML += `< class="card-works1">
             <div class="snap-shoot">
               <img
                 src="${card.image}"
@@ -81,13 +82,10 @@ cards.map(
             </div>
             <div class="left-block">
               <h2 id="title">${card.title}</h2>
-              <ul class="frmae">
+              <ul class="frame">
               ${card.roles
-      .map(
-        (role, idx) => ` <li class="client">${role}</li>
-                <li class="counter"><img src="images 1/Counter (1).png" id="dot-${idx}" /></li>`,
-      )
-      .join('')}
+                .map((role, idx) => ` <li class="client">${role}</li>`)
+                .join("")}
               </ul>
             
             <p id="main-text1">
@@ -95,16 +93,15 @@ cards.map(
             </p>
             <ul class="tags">
               ${card.languages
-      .map((lang) => `<li id="tag1">${lang}</li>`)
-      .join('')}
+                .map((lang) => `<li id="tag1">${lang}</li>`)
+                .join("")}
             </ul>
             <div class="normal-button">
               <a href="javascript:openModal(${index})" id="view1">See Project</a>
             </div>
             </div>
-          </div>
         </div>
-      `),
+      `)
 );
 
 const openModal = (index) => {
@@ -113,13 +110,13 @@ const openModal = (index) => {
   longDes.innerHTML = cards[index].longDes;
   live.href = cards[index].live;
   source.href = cards[index].source;
-  modal.style.display = 'block';
-  body.style.overflow = 'hidden';
+  modal.style.display = "block";
+  body.style.overflow = "hidden";
 };
 
 const closeModal = () => {
-  modal.style.display = 'none';
-  body.style.overflow = 'auto';
+  modal.style.display = "none";
+  body.style.overflow = "auto";
 };
 
 openModal();
