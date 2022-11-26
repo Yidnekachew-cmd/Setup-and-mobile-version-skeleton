969f74bad20fe5a06d4948d92d0dd7555ffdf1da
Yidnekachew Kassahun Kibret <yamiyekn@gmail.com>
2022-11-26 21:15:16 +0300

    Add fixed linter error

--- JavaScript/form.js
+++ JavaScript/form.js
@@ -1,17 +1,17 @@
 function emailValidator() {
-  const EMAIL_INVALID = "Please enter a correct email address format";
-  const email = document.getElementById("email").value;
+  const EMAIL_INVALID = 'Please enter a correct email address format';
+  const email = document.getElementById('email').value;
   const emailRegx = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
 
   document.forms[0].onsubmit = function (event) {
     if (emailRegx.test(email)) {
-      document.getElementById("message").innerHTML = "Success!";
-      document.getElementById("message").style.color = "green";
-      document.getElementById("message").style.fontSize = "24px";
+      document.getElementById('message').innerHTML = 'Success!';
+      document.getElementById('message').style.color = 'green';
+      document.getElementById('message').style.fontSize = '24px';
     } else {
-      document.getElementById("message").innerHTML = EMAIL_INVALID;
-      document.getElementById("message").style.color = "red";
-      document.getElementById("message").style.fontSize = "24px";
+      document.getElementById('message').innerHTML = EMAIL_INVALID;
+      document.getElementById('message').style.color = 'red';
+      document.getElementById('message').style.fontSize = '24px';
       event.preventDefault();
     }
   };
